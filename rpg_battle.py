5fde3ed41da9e3a63b2c80937d9f9ba017be0431
sazitor <sazika624@gmail.com>
2026-07-16 11:33:47 +0900

    命中率・回避率システムを追加

--- rpg_battle.py
+++ rpg_battle.py
@@ -1,19 +1,21 @@
 import random
 
 class Character:
-    def __init__(self, name, hp, attack, crt=0.2, escape_rate=0.5, hold_rate=0.5):
+    def __init__(self, name, hp, attack, crt=0.2, escape_rate=0.5, hold_rate=0.5, accuracy=0.9, evasion=0.1):
         self.name = name
-        self.hp = hp
-        self.max_hp = hp
-        self.attack = attack
+        self.max_hp = hp    
+        self.hp = hp    
+        self.attack = attack  
         self.crt = crt
         self.escape_rate = escape_rate
         self.hold_rate = hold_rate
+        self.accuracy = accuracy  # 命中率
+        self.evasion = evasion    # 回避率
 
-player1 = Character("プレイヤー", 100, 20,crt = 0.3,escape_rate = 1)    
-enemy1 = Character("スライム", 80, 15,crt = 0.2,hold_rate = 0.2)
-enemy2 = Character("ゴブリン", 60, 18,crt = 0.35,hold_rate = 0.5)
-enemy3 = Character("ドラゴン", 150, 25,crt = 0.2,hold_rate = 0.7)
+player1 = Character("プレイヤー", 100, 20,crt = 0.3,escape_rate = 1,hold_rate = 0.5,accuracy = 0.9,evasion = 0.1)    
+enemy1 = Character("スライム", 80, 15,crt = 0.2,hold_rate = 0.2,accuracy = 0.9,evasion = 0.1)
+enemy2 = Character("ゴブリン", 60, 18,crt = 0.35,hold_rate = 0.5,accuracy = 0.9,evasion = 0.1)
+enemy3 = Character("ドラゴン", 150, 25,crt = 0.2,hold_rate = 0.7,accuracy = 0.9,evasion = 0.1)
 
 enemy = random.choice([enemy1, enemy2, enemy3])
 print(enemy.name+"が現れた！")
@@ -24,17 +26,21 @@ while True:
     en_crt = False
 
     if command == "1":
-        if random.random() < player1.crt:
-            is_crt = True
-        if is_crt == True:
-            print(player1.name+"のクリティカル！")
-            enemy.hp -= player1.attack * 2
-            print(player1.name +"の攻撃"+enemy.name+"に"+str(player1.attack * 2)+"のダメージ！")
-            print(enemy.name+"の残りHP:"+str(enemy.hp))
-        else:
-            enemy.hp -= player1.attack
-            print(player1.name +"の攻撃"+enemy.name+"に"+str(player1.attack)+"のダメージ！")
-            print(enemy.name+"の残りHP:"+str(enemy.hp))
+        accuracy_check = random.random()
+        if accuracy_check > player1.accuracy - enemy.evasion:
+            print(player1.name+"の攻撃は外れた！")
+        else:  
+            if random.random() < player1.crt:
+                is_crt = True
+            if is_crt == True:
+                print(player1.name+"のクリティカル！")
+                enemy.hp -= player1.attack * 2
+                print(player1.name +"の攻撃"+enemy.name+"に"+str(player1.attack * 2)+"のダメージ！")
+                print(enemy.name+"の残りHP:"+str(enemy.hp))
+            else:
+                enemy.hp -= player1.attack
+                print(player1.name +"の攻撃"+enemy.name+"に"+str(player1.attack)+"のダメージ！")
+                print(enemy.name+"の残りHP:"+str(enemy.hp))
     if command == "2":
         print(player1.name+"は防御した！")
         gurad = True
@@ -56,24 +62,25 @@ while True:
         else:
             print(player1.name+"は逃げられなかった！")
     if enemy.hp > 0:
-        if random.random() < enemy.crt:
-            en_crt = True
+        enemy_accuracy_check = random.random()
+        if enemy_accuracy_check > enemy.accuracy - player1.evasion:
+            print(enemy.name+"の攻撃は外れた！")
+        else:   
+            if random.random() < enemy.crt:
+                en_crt = True
         
-        damage = enemy.attack
-        if en_crt == True:
-            print(enemy.name+"のクリティカル！")
-            damage = damage * 2
-        if gurad == True:
-            damage = int(damage / 2)
-        player1.hp -= damage
+            damage = enemy.attack
+            if en_crt == True:
+                print(enemy.name+"のクリティカル！")
+                damage = damage * 2
+            if gurad == True:
+                damage = int(damage / 2)
+            player1.hp -= damage
+       
+            print(enemy.name +"の攻撃")
 
-
-
-
-
-        
-        print(enemy.name +"の攻撃"+player1.name+"に"+ str(damage)+"のダメージ！")
-        print(player1.name+"の残りHP:"+str(player1.hp))
+            print(player1.name+"に"+ str(damage)+"のダメージ！")
+            print(player1.name+"の残りHP:"+str(player1.hp))
     if player1.hp <= 0:
         print(player1.name+"は倒された…")
         break
